ef4a023afe8308a3af1c7e8127b38a476e185e76
Oracene Adam <ST10225138@rcconnect.edu.za>
2023-04-27 20:52:54 +0000

    PART 1[PROG6221POE]

--- Program.cs
+++ Program.cs
@@ -1,100 +1,143 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
-class Ingredient
+//class created for ingredients
+class Ingredients
 {
     public string Name { get; set; }
     public decimal Quantity { get; set; }
     public string Unit { get; set; }
 
-    public Ingredient(string name, decimal quantity, string unit)
+    public Ingredients(string name, decimal quantity, string unit)
     {
         Name = name;
         Quantity = quantity;
         Unit = unit;
     }
-
+    //overrides the difference in 'input' data types to displaying outputs as strings(ALL)
     public override string ToString()
     {
         return Name + " " + Quantity + " " + Unit;
     }
-
+    //method to calculate scaled ingredient quantities
     public void Scale(decimal factor)
     {
-        Quantity *= factor;
+        Quantity = Quantity * factor;
     }
 }
-
-class Recipe
+internal class Recipe
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public List<Ingredient> Ingredients { get; set; }
+    public int numOfIngr { get; set; }
+    public int numOfSteps { get; set; }
+    public List<Ingredients> Ingredients { get; set; }
+    public string[] Steps { get; set; }
 
-    public Recipe(string name, string description, List<Ingredient> ingredients)
+    public Recipe(int ingrNum, int numSteps, List<Ingredients> ingredients, string[] steps)
     {
-        Name = name;
-        Description = description;
+        numOfIngr = ingrNum;
+        numOfSteps = numSteps;
         Ingredients = ingredients;
+        Steps = steps;
     }
-
-    public void Display()
+    //below: method for displaying user's input
+    public void RecipeDisplay()
     {
-        Console.WriteLine("Name: " + Name);
-        Console.WriteLine("Description: " + Description);
-        Console.WriteLine("Ingredients:");
-        foreach (Ingredient ingredient in Ingredients)
+        Console.WriteLine("Ingredients: ");
+        foreach (Ingredients ingredients in Ingredients)
         {
-            Console.WriteLine("- " + ingredient);
+            Console.WriteLine("- " + ingredients);
+        }
+        Console.WriteLine("Steps: ");
+        for (int i = 0; i < Steps.Length; i++)
+        {
+            Console.WriteLine((i + 1) + ". " + Steps[i]);
         }
     }
-
-    public void Scale(decimal factor, string ingredientName)
+    public void Clear()
     {
-        foreach (Ingredient ingredient in Ingredients)
+        Recipe.ReferenceEquals(RecipeDisplay, null);
+
+    }
+}
+        //method that enables user to factor their ingredient quantities
+
+        //user input prompting is placed within the main class for execution
+        class RecipeProgram
         {
-            if (ingredient.Name == ingredientName)
+            static void Main()
             {
-                ingredient.Scale(factor);
+                //displaying info to user on the purpose of the application
+                Console.WriteLine("Hello User :) !!! ");
+                Console.WriteLine("WELCOME TO OUR RECIPE APP." +
+                    "This application allows you to store information for only ONE recipe. To begin, enter the following:  ");
+                Console.WriteLine("\n");
+
+                //Promp user for :
+                // number of ingredients
+                Console.WriteLine("Enter recipe information:");
+
+                Console.Write("Number of ingredients: ");
+                int numIngredients = int.Parse(Console.ReadLine());
+                List<Ingredients> ingredients = new List<Ingredients>();
+                for (int i = 0; i < numIngredients; i++)
+                {
+                    Console.Write("Ingredient " + (i + 1) + " name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Quantity: ");
+                    decimal quantity = decimal.Parse(Console.ReadLine());
+                    Console.Write("Unit: ");
+                    string unit = Console.ReadLine();
+                    ingredients.Add(new Ingredients(name, quantity, unit));
+                }
+                Console.WriteLine("Input number of steps: ");
+                int numOfSteps = int.Parse(Console.ReadLine());
+                string[] steps = new string[numOfSteps];
+                for (int i = 0; i < numOfSteps; i++)
+                {
+                    Console.Write("Enter description of step " + (i + 1) + ": ");
+                    steps[i] = Console.ReadLine();
+                }
+
+                Recipe recipe = new Recipe(numIngredients, numOfSteps, ingredients, steps);
+                Console.WriteLine("\nRecipe created:");
+                recipe.RecipeDisplay(); //method that displays user's recipe in full
+
+                Console.Write("Enter an ingredient to scale: ");
+                string ingredientName = Console.ReadLine();
+                //Prompt user for a scaling factor to scale their recipe
+                Console.Write("Enter a scaling factor (0.5, 1, 2, or 3): ");
+                decimal factor = decimal.Parse(Console.ReadLine());
+                //outer loop with inner if statement to scale an ingredient by a factor
+                foreach (Ingredients ingredient in recipe.Ingredients)
+                {
+                    if (ingredient.Name == ingredientName)
+                    {
+                        ingredient.Scale(factor);
+                    }
+                }
+                Console.WriteLine("\nRecipe scaled by a factor of " + factor + ":");
+                Console.WriteLine("\nNew Recipe after scaling: ");
+                recipe.RecipeDisplay();
+                //prompt for user's option to input another recipe or exit application
+                Console.WriteLine("Would you like to enter a NEW recipe? If yes, type (YES) or press (ENTER) to exit: ");
+                string userInput = Console.ReadLine();
+                if (userInput == "YES")
+                {
+                    
+                    Console.WriteLine($"\n"); //sentencing spacing for user visibility.
+                    Main(); //calling the main method that enables user to re-enter their recipe 
+                }
+                else
+                {
+                    Console.WriteLine("You've reached the end of this application. Thanks a mil for using our RECIPE APP, we " +
+                        "greatly appreciate your time. GOODBYE!!!" +
+                        ":-) ");
+                }
             }
-        }
-    }
-}
+        } 
+    
 
-class RecipeProgram
-{
-    static void Main()
-    {
-        Console.WriteLine("Enter recipe information:");
-        Console.Write("Name: ");
-        string name = Console.ReadLine();
-        Console.Write("Description: ");
-        string description = Console.ReadLine();
-        Console.Write("Number of ingredients: ");
-        int numIngredients = int.Parse(Console.ReadLine());
-        List<Ingredient> ingredients = new List<Ingredient>();
-        for (int i = 0; i < numIngredients; i++)
-        {
-            Console.Write("Ingredient " + (i + 1) + " name: ");
-            string ingredientName = Console.ReadLine();
-            Console.Write("Quantity: ");
-            decimal quantity = decimal.Parse(Console.ReadLine());
-            Console.Write("Unit: ");
-            string unit = Console.ReadLine();
-            ingredients.Add(new Ingredient(ingredientName, quantity, unit));
-        }
 
-        Recipe recipe = new Recipe(name, description, ingredients);
-        Console.WriteLine("\nRecipe created:");
-        recipe.Display();
-
-        Console.Write("Enter an ingredient to scale: ");
-        string ingredientToScale = Console.ReadLine();
-        Console.Write("Enter a scaling factor (0.5, 1, 2, or 3): ");
-        decimal factor = decimal.Parse(Console.ReadLine());
-        recipe.Scale(factor, ingredientToScale);
-        Console.WriteLine("\nRecipe scaled by a factor of " + factor + " for " + ingredientToScale + ":");
-        recipe.Display();
-    }
-}
 
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# MyRecipeApp_POE
+# MyRecipeApp_POE Part 1 by ORACENE ADAM[ST10225138]
+
+##How to run this application
+1. Open the solution file in Visual Studio.
+2. Build the solution.
+3. Run the app by pressing the F5 key or click the START button('play icon') in the toolbar.
+
+##How to use the RECIPE APP
+1. After clicking start, a console terminal will pop-up displaying a welcome message and definition of the app's purpose.
+2. Then, it will prompt you to enter the number of ingredients in your desired recipe.
+3. Another prompt to input an ingredient's 'name', 'quantity', & 'unit of measurement' for every ingredient base on the number of ingredients entered in the previous prompt.
+5. Next, the app will prompt for number of steps and steps descriptions.
+6. Once the above is entered, the app will display the recipe in correspondance to your inputs.
+7. You then will be asked to scale ingredient quantities by either 0.5, 2 or 3.
+8. The app shall display and update your recipe according to the scale factor entered.
+9. Thereafter, you will prompted to either enter a new recipe or exit the application.
+
+##File Structure
+~README.md : defines the entire code and coding structures in a more readable way for non-IT personals.
+~Program.cs : Contains three major classes: 1. Ingredients --> defines a class that encapsulates user input & scaling functionality.
+											2. Recipe --> defines the recipe class which stores the app's procedural functions.
+											3. RecipeProgram --> consists of the main method, which contains the app's program logic.
+
+##Dependencies
+Application has no external dependencies beyound the standard .NET libraries.
+
+##Known issues
+none at this point in time.
+
+##Contritubutions
+This is a project/assignment with intentions to help me gain coding experience, therefore contributions are welcomed
+Please submit any issues / pull requests to my Github repository
+
+##License
+This project is a educational Potifolio Of Evidence. No 'LICENSE is necessary at this time.
